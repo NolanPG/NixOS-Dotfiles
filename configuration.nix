41c4fc5342f1cae64414c7371a486e2a397a2c2c
NolanPG <nolanperez35@gmail.com>
2024-05-16 19:12:03 +0200

    Removed silent boot

--- configuration.nix
+++ configuration.nix
@@ -35,12 +35,6 @@
         efiSysMountPoint = "/boot";
       };
     };
-
-    # Modifications for a completely silent boot
-    initrd.verbose = false;
-    consoleLogLevel = 0;
-    kernelParams = [ "quiet" "udev.log_level=3" ];
-    plymouth.enable = true;
   };
 
   # Enabling SOUND
